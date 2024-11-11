c53fd67eee0b35472c49fdad38c356ad95b69463
Brian Vega <dentvega6@gmail.com>
2024-11-11 16:23:20 -0400

    refactor de las keys de firebase web

--- firebase.Config.js
+++ firebase.Config.js
@@ -1,16 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-
-const firebaseConfig = {
-  apiKey: "apiKey",
-  authDomain: "authDomain",
-  projectId: "projectId",
-  storageBucket: "storageBucket",
-  messagingSenderId: "messagingSenderId",
-  appId: "appId",
-  measurementId: "measurementId"
-};
+import firebaseConfig from '@/firebase-services';
 
 const app = initializeApp(firebaseConfig);
 
